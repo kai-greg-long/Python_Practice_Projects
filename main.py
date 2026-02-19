99df70f6a81bde4b600f220eebff1644035a0b79
Kai Long <12345kai.long@gmail.com>
2026-02-18 21:13:11 -0800

    Currency Converter Completed

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+
+base_url= 'https://api.fxratesapi.com/latest'
+currency = input('Enter currency code: ').upper()
+amount = float(input('Enter amount: '))
+target = input('Enter target currency code: ').upper()
+
+def convert(amount, currency, target):
+    response = requests.get(base_url, params={'base': currency})
+    data = response.json()
+    rate = data['rates'][target]
+    converted = amount * rate
+    print(f'{amount} in {currency} equals {converted} in {target}')
+
+convert(amount, currency, target)
+
+
